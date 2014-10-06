684b6c6faeb03e9991f5be01cbfa95f0ab8d462f
Greg Monson <gregorymonson@gmail.com>
2014-10-06 00:17:50 -0700

    scheme compiler

---
+++ hw14.py
@@ -0,0 +1,35 @@
+# Name:
+# Login:
+# TA:
+# Section:
+# Q1.
+
+def featherweight():
+    """Return the integer number of your favorite featherweight entry.
+
+    >>> isinstance(featherweight(), int)
+    True
+    >>> 15 >= featherweight() >= 0
+    True
+    """
+    return 14  # Change to a non-negative integer
+
+def heavyweight():
+    """Return the integer number of your favorite heavyweight entry.
+
+    >>> isinstance(heavyweight(), int)
+    True
+    >>> 23 >= heavyweight() >= 18
+    True
+    """
+    return 18  # Change to a non-negative integer
+
+# These lines will be used to tally your vote.  Please do not change them.
+print(featherweight())
+print(heavyweight())
+
+
+# Q2.
+
+# Fill out the survey at http://goo.gl/3Oo4N to receive credit for this
+# question.
